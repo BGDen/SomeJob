86fa82a19b4c4df3ba98b27061558952163252fe
Den <fentau.line@gmail.com>
2020-04-24 18:30:43 +0300

    Updates

---
+++ precache-manifest.39774314f18a506bc99ff5a7fd927661.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ebae82a8321b0edebaf87dc9ebe5c7e",
+    "url": "/SomeJob/index.html"
+  },
+  {
+    "revision": "00fcaea0aebb2c67c4e6",
+    "url": "/SomeJob/static/css/main.42f47351.chunk.css"
+  },
+  {
+    "revision": "0fdad72ef48b764cc0c8",
+    "url": "/SomeJob/static/js/2.96cf86ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SomeJob/static/js/2.96cf86ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00fcaea0aebb2c67c4e6",
+    "url": "/SomeJob/static/js/main.4dd954f5.chunk.js"
+  },
+  {
+    "revision": "b6cd8e60b89e416a8716",
+    "url": "/SomeJob/static/js/runtime-main.a5da3d89.js"
+  }
+]);
